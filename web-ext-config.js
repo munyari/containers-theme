da3c64388d7687216b733c650702011dd5c854ae
Panashe M. Fundira <panashe@dropbox.com>
2020-05-23 15:17:23 -0400

    Prettier

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,21 +1,20 @@
 /* eslint-env node */
 
 const defaultConfig = {
-	// Global options:
-	sourceDir: "./src/",
-	artifactsDir: "./dist/",
-	ignoreFiles: [".DS_Store"],
-	// Command options:
-	build: {
-		overwriteDest: true,
-	},
-	run: {
-		firefox: "nightly",
-		browserConsole: true,
-		startUrl: ["about:debugging"],
-		pref: [],
-	},
+  // Global options:
+  sourceDir: "./src/",
+  artifactsDir: "./dist/",
+  ignoreFiles: [".DS_Store"],
+  // Command options:
+  build: {
+    overwriteDest: true
+  },
+  run: {
+    firefox: "firefoxdeveloperedition",
+    browserConsole: true,
+    startUrl: ["about:debugging"],
+    pref: []
+  }
 };
 
 module.exports = defaultConfig;
-
